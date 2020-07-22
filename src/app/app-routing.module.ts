9f571555446eecd27d65938ffe863c26cd598d62
josepereza <josepereza66@gmail.com>
2020-07-22 14:47:53 +0200

    first3 commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
     {path: 'post',
     loadChildren: () => import('./post/post.module').then(m => m.PostModule)},
-    { path: 'post2', loadChildren: () => import('./post2/post2.module').then(m => m.Post2Module) }
 ];
 
 @NgModule({
